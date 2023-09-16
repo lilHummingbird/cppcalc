13a064475c3e46011a9f6327ddd96afa75f721a5
Hummingbird <stphpedersen@gmail.com>
2023-09-16 21:45:42 +0200

    Initial Commit

---
+++ cppcalc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppcalc
+//
+//  Created by Persephone on 16/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
