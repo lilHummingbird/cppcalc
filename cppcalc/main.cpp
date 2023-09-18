d4d1dd11a17a1de107e6bdf9d1b3de3f5026f2b3
Persephone <143192951+lilHummingbird@users.noreply.github.com>
2023-09-18 19:57:17 +0200

    Conflic fix
    Don't get why it wanted to say no so i just fixed it like this instead

--- cppcalc/main.cpp
+++ cppcalc/main.cpp
@@ -9,13 +9,17 @@
 #include <iostream>
 #include "main.h"
 
-/// XCode Git Test
-
+void inputText() {
+    
+    std::cout << "Input your number(s) one at a time pressing enter inbetween them!" << std::endl;
+    
+    return;
+}
 
 int main() {
 start:
 
-    std::cout << "Please enter the number corresponding to what you want to do!\n" << "1. Add\n" << "2. Subtract\n" << "3. Multiply\n" << "4. Divide\n" << "5. Square root\n" << "6. Power\n"
+    std::cout << "Please enter the number corresponding to what you want to do!\n" << "1. Add\n" << "2. Subtract\n" << "3. Multiply\n" << "4. Divide\n" << "5. Power\n" << "6. Square root\n"
         << "7. Exit\n" << std::endl;
 
     int x;
@@ -26,14 +30,51 @@ start:
 
     switch (x)
     {
-    case 1:
-       std::cout << "Input your numbers one at a time pressing enter inbetween them!" << std::endl;
-       std::cout << add(input(),input()) << std::endl;
+        case 1:
 
-        goto start;
-
-    default:
-        break;
+            inputText();
+            std::cout << add(input(), input()) << std::endl;
+            
+            goto start;
+        case 2:
+            
+            inputText();
+            std::cout << subtract(input(), input()) << std::endl;
+            
+            goto start;
+        case 3:
+            
+            inputText();
+            std::cout << multiply(input(), input()) << std::endl;
+            
+            goto start;
+        case 4:
+            
+            inputText();
+            std::cout << divide(input(), input()) << std::endl;
+            
+            goto start;
+        case 5:
+            
+            inputText();
+            std::cout << power(input(), input()) << std::endl;
+            
+            goto start;
+        case 6:
+            
+            inputText();
+            std::cout << sqre(input()) << std::endl;
+            
+            goto start;
+        case 7:
+            
+            std::cout << "Thank you for using my calculator" << std::endl;
+            
+            break;
+        default:
+            std::cout << "Invalid input" << std::endl;
+            
+            goto start;
     }
     return 0;
 }
